88d13b6bae572a7391fecb6f56fae457404e93a3
abhinavtomar2912-dot <abhinavtomar2912@gmail.com>
2026-02-25 15:47:22 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,139 @@
+############################
+# TERRAFORM BACKEND (S3)
+############################
+terraform {
+  backend "s3" {
+    bucket         = "amzn-s3-new-bkt"   # must already exist
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+############################
+# PROVIDER
+############################
+provider "aws" {
+  region = "us-east-1"
+}
+
+############################
+# IAM ROLE FOR EKS CLUSTER
+############################
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+############################
+# DATA: DEFAULT VPC + SUBNETS
+############################
+data "aws_vpc" "existing" {
+  default = true
+}
+
+data "aws_subnets" "existing" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing.id]
+  }
+}
+
+############################
+# EKS CLUSTER
+############################
+resource "aws_eks_cluster" "eks" {
+  name      = "my_cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = data.aws_subnets.existing.ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+############################
+# EKS ADDONS
+############################
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "kube-proxy"
+}
+
+############################
+# IAM ROLE FOR NODE GROUP
+############################
+resource "aws_iam_role" "node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+############################
+# EKS NODE GROUP
+############################
+resource "aws_eks_node_group" "nodes" {
+  cluster_name   = aws_eks_cluster.eks.name
+  node_role_arn = aws_iam_role.node_role.arn
+  subnet_ids    = data.aws_subnets.existing.ids
+
+  instance_types = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_policy
+  ]
+}
